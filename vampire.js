8699607beacd6fa2ea3faacd550f0cb52af2c43a
Anastasia <z.anastasia11@gmail.com>
2023-05-18 12:49:21 -0700

    implement recursive functions

--- vampire.js
+++ vampire.js
@@ -8,44 +8,52 @@ class Vampire {
 
   /** Simple tree methods **/
 
-  // Adds the vampire as an offspring of this vampire
+  //1 Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
-  // Returns the total number of vampires created by that vampire
+  // 2 Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
-  // Returns the number of vampires away from the original vampire this vampire is
+  // 3 Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numOfVamp = 0;
+    let currentVamp = this;
+   
+    while (currentVamp.creator) {
+    
+      currentVamp = currentVamp.creator;
+      numOfVamp++;
+    }
+    return numOfVamp;
   }
 
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  // 4 Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    // Count the number of ancestors in the lineage of each vampire
+    let numOfAncestors = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numOfAncestors++;
+    }
 
+    let numOfAncestorsOther = 0;
+    let otherVamp = vampire;
+    while (otherVamp.creator) {
+      otherVamp = otherVamp.creator;
+      numOfAncestorsOther++;
+    }
+
+    // Compare the number of ancestors and return true if the current vampire has fewer
+    return numOfAncestors < numOfAncestorsOther;
   }
 
-  /** Tree traversal methods **/
-
-  // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
-    
-  }
-
-  // Returns the total number of vampires that exist
-  get totalDescendents() {
-    
-  }
-
-  // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
-
-  /** Stretch **/
+  /** 5 Stretch **/
 
   // Returns the closest common ancestor of two vampires.
   // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
@@ -55,7 +63,53 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
+
+
+  //RECURSIVE FUNCTIONS
+  
+  // 6  Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (name === this.name) {
+      return this;
+    }
+
+    for (const offspring of this.offspring) {
+      if (offspring.vampireWithName(name)!== null)
+      return offspring.vampireWithName(name);
+    }
+    return null;
+  }
+
+  //7 Returns the total number of vampires that exist
+  get totalDescendents() {
+    let totalDes = 0; 
+
+    for (const offspring of this.offspring) {
+      totalDes += offspring.totalDescendents + 1;
+    }
+    return totalDes;
+  }
+  
+
+  //8 Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let totalMillenial = [];
+
+    if (this.yearConverted > 1980) {
+      totalMillenial.push(this);
+    }
+
+    for (const offspring of this.offspring) {
+      totalMillenial = totalMillenial.concat(offspring.allMillennialVampires)
+        
+    }
+    return totalMillenial;
+  }
+
+ 
 }
 
 module.exports = Vampire;
+
+
 
